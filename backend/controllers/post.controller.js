34e2c09942a8171980cf08bc32043a3b1a242100
Trishir08 <trishirbudhiraja003@gmail.com>
2024-06-20 00:15:43 +0530

    created post routes

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import Post from "../models/posts.model.js";
 import User from "../models/user.model.js";
 import { v2 as cloudinary } from "cloudinary";
@@ -26,7 +27,7 @@ export const createPost = async (req,res)=>{
         }) ; 
 
         await newPost.save() ; 
-        res.status(201).json({message : "Post created successfully"}) ; 
+        res.status(201).json(newPost);
 
 
     }catch(error){
@@ -75,7 +76,7 @@ export const commentOnPost = async(req , res) => {
         post.comment.push(com) ; 
         await post.save() ; 
 
-        res.status(201).json({message : "Comment added successfully"}) ; 
+        res.status(200).json(post);
 
     }catch(error){
         console.log(`Error in commentOnPost :- ${error.message}`);
@@ -85,12 +86,147 @@ export const commentOnPost = async(req , res) => {
 
 
 export const likeUnlikePost = async (req ,res)=>{
+    
     try{
-        false
-        DataTran
+        const userId = req.user._id ; 
+        const {id: postId} = req.params ; 
+        
+        const post = await Post.findById(postId) ; 
+        if(!post) return res.status(404).json({message : "Post not found"}) ; 
 
+        const isLiked = post.likes.includes(userId) ; 
+
+        if(isLiked){
+            // already liked 
+            await Post.updateOne({_id : postId} , {$pull : {likes : userId}}) ;
+            await User.updateOne({_id : userId }, {$pull : {likedPosts : postId}}) ; 
+            res.status(201).json({message : "Post Unliked successfully"}) ; 
+        }else{
+            post.likes.push(userId) ; 
+            await post.save() ; 
+            await User.updateOne({_id : userId }, {$push : {likedPosts : postId}}) ; 
+            const notification = new Notification({
+                from : userId , 
+                to : post.user  , 
+                type : "like"
+            }) ; 
+            await notification.save() ; 
+            // always save the notification
+            const updatedLikes = post.likes;
+			res.status(200).json(updatedLikes); 
+        }
+        
     }catch(error){
         console.log(`Error in likeUnlikePost :- ${error.message}`);
         res.status(500).json({error : "Internal server error"}) ; 
     }
+}
+
+
+export const getAllPosts = async (req , res)=>{
+    try{
+
+        const posts = await Post.find()
+			.sort({ createdAt: -1 })
+			.populate({
+				path: "user",
+				select: "-password",
+			})
+			.populate({
+				path: "comments.user",
+				select: "-password",
+			});
+
+		if (posts.length === 0) {
+			return res.status(200).json([]);
+		}
+
+		res.status(200).json(posts);
+
+    }catch(error){
+        console.log(`Error in getAllPosts :- ${error.message}`);
+        res.status(500).json({error : "Internal server error"}) ; 
+    }
+}
+
+export const getLikedPosts = async (req,res)=>{
+
+    const userId = req.params.id ; 
+
+    try{
+    
+        const user = await User.findById(userId) ; 
+        if(!user) return res.status(404).json({message : "User not found"}) ;
+
+        const likedPosts = await Post.find({ _id: { $in: user.likedPosts } })
+			.populate({
+				path: "user",
+				select: "-password",
+			})
+			.populate({
+				path: "comments.user",
+				select: "-password",
+			});
+
+		res.status(200).json(likedPosts);
+
+    }catch(error){
+        console.log(`Error in getAllPosts :- ${error.message}`);
+        res.status(500).json({error : "Internal server error"}) ; 
+    }
+}
+
+export const getFollowingPosts = async (req , res)=>{
+    try{
+
+        const userId = req.user._id ; 
+        const user = await User.findById(userId) ; 
+
+        if(!user) return res.status(404).json({message : "User not found"}) ; 
+
+        const following = user.following ; 
+
+        const feedPosts = (await Post.find({user : {$in : following}})).
+        sort({createdAt : -1})
+        .populate({
+            path : "user" , 
+            select : "-password" , 
+        })
+        .populate({
+            path : "comments.user" , 
+            select : "-password" 
+        }) ;
+
+        res.status(200).json(feedPosts) ; 
+
+    }catch(error){
+        console.log(`Error in getFollowingPosts :- ${error.message}`);
+        res.status(500).json({error : "Internal server error"}) ;
+    }
+}
+
+
+export const getUserPosts = async (req, res) => {
+	try {
+		const { username } = req.params;
+
+		const user = await User.findOne({ username });
+		if (!user) return res.status(404).json({ error: "User not found" });
+
+		const posts = await Post.find({ user: user._id })
+			.sort({ createdAt: -1 })
+			.populate({
+				path: "user",
+				select: "-password",
+			})
+			.populate({
+				path: "comments.user",
+				select: "-password",
+			});
+
+		res.status(200).json(posts);
+	} catch (error) {
+		console.log("Error in getUserPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
 }
--- backend/routes/post.router.js
+++ backend/routes/post.router.js
@@ -1,6 +1,6 @@
 import express from "express" 
 import { protectRoute } from "../middleware/protectedRoute.js";
-import { commentOnPost, createPost, deletePost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost , likeUnlikePost , getAllPosts , getUserPosts,  getLikedPosts , getFollowingPosts } from "../controllers/post.controller.js";
 
 const router = express.Router() ;
 
@@ -8,6 +8,9 @@ router.post('/create' , protectRoute , createPost) ;
 router.delete('/:id' , protectRoute , deletePost) ; 
 router.post('/comment/:id' , protectRoute , commentOnPost) ; 
 router.post('/like/:id' , protectRoute , likeUnlikePost) ; 
-
+router.get('/all' , protectRoute , getAllPosts) ; 
+router.get('/likes/:id' , protectRoute , getLikedPosts) ; 
+router.get('/following' , protectRoute , getFollowingPosts) ; 
+router.get('/user/:username' , protectRoute , getUserPosts) ; 
 
 export default router ; 
